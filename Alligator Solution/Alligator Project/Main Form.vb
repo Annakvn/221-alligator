8b1156ba9c240bd96e07cf0d6fcaa48471e5dd40
Annakvn <38144067+Annakvn@users.noreply.github.com>
2019-09-11 00:19:06 -0700

    upload

---
+++ Alligator Solution/Alligator Project/Main Form.vb
@@ -0,0 +1,36 @@
+﻿'Project name:          Alligator Project
+'Project purpose:       Display total annual allowance
+'Created/revised by:    Anna Kovneva by February 16,2017
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public Class MainForm
+    Private Sub printPreviewButton_Click(sender As Object, e As EventArgs) Handles printPreviewButton.Click
+        PrintForm1.Print()
+
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub calcButton_Click(sender As Object, e As EventArgs) Handles calcButton.Click
+        'calculate and display the total annnual allowance
+
+        Const MonthlyAllowance As Integer = 200
+        Dim people As Integer
+        Dim annualAllow As Integer
+
+        'store user input in a variable
+        Integer.TryParse(peopleTextBox.Text, people)
+
+        'calculate the total annual allowance
+        annualAllow = people * MonthlyAllowance * 12
+
+        'display total annual allowance 
+        totalLabel.Text = annualAllow.ToString("C0")
+    End Sub
+End Class
